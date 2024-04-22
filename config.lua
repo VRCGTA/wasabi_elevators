9a4d974654563b356a5dfe27722704a19d0ff10a
azutake <azusa_takemae@dwango.co.jp>
2024-04-22 16:28:52 +0900

    add floor

--- config.lua
+++ config.lua
@@ -9,6 +9,20 @@ Config.checkForUpdates = true -- Check for Updates?
 Config.Elevators = {
     PillboxElevatorNorth = { -- Elevator name(Doesn't show it's just to name table)
         [1] = {
+            coords = vec3(355.65, -595.73, 28.78), -- Coords, if you're new; last number is heading
+            heading = 70.65, -- Heading of how will spawn on floor
+            title = 'Car port', -- Title 
+            description = 'car port', -- Description
+            target = { -- Target length/width
+                width = 5,
+                length = 4
+            },
+            groups = {-- Job locks
+                'police',
+                'ambulance'
+            },
+        },
+        [2] = {
             coords = vec3(325.47, -599.04, 43.29), -- Coords, if you're new; last number is heading
             heading = 70.65, -- Heading of how will spawn on floor
             title = 'Main Floor', -- Title 
@@ -22,7 +36,7 @@ Config.Elevators = {
                 'ambulance'
             },
         },
-        [2] = {
+        [3] = {
             coords = vec3(338.57, -583.93, 74.17), -- Coords, if you're new; last number is heading
             heading = 252.84,
             title = 'Roof floor',
